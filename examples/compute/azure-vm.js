d860d5bcc27426c2bf159fccdd3e9f3fcd5cd62d
Rajendra Kadam <rajendrakadam249@gmail.com>
2018-05-25 19:39:46 +0530

    Add storage example

--- examples/compute/azure-vm.js
+++ examples/compute/azure-vm.js
@@ -1,49 +1,65 @@
-const nodeCloud = require('../../lib/');
+const nodeCloud = require("../../lib/");
 
 const azure = nodeCloud.getProvider("Azure", null);
 
 const vm = azure.compute();
 
-const resourceGroupName = 'nodecloud';
-const vmName = 'nodecloud-test';
-const publisher = 'Canonical';
-const offer = 'UbuntuServer';
-const sku = '14.04.3-LTS';
-const osType = 'Linux';
+const resourceGroupName = "nodecloud";
+const vmName = "nodecloud-test";
+const publisher = "Canonical";
+const offer = "UbuntuServer";
+const sku = "14.04.3-LTS";
+const osType = "Linux";
 const params = {
-    location: 'centralus',
-    osProfile: {
-      computerName: vmName,
-      adminUsername: 'ubuntuServer',
-      adminPassword: 'Pa$$w0rd92'
-    },
-    hardwareProfile: {
-      vmSize: 'Basic_A0'
-    },
-     storageProfile: {
-      imageReference: {
-        publisher: publisher,
-        offer: offer,
-        sku: sku,
-        version: 'latest'
+  location: "centralus",
+  osProfile: {
+    computerName: vmName,
+    adminUsername: "ubuntuServer",
+    adminPassword: "Pa$$w0rd92"
+  },
+  hardwareProfile: {
+    vmSize: "Basic_A0"
+  },
+  storageProfile: {
+    imageReference: {
+      publisher: publisher,
+      offer: offer,
+      sku: sku,
+      version: "latest"
+    }
+  },
+  networkProfile: {
+    networkInterfaces: [
+      {
+        id:
+          "/subscriptions/" +
+          process.env.AZURE_SUBSCRIPTION_ID +
+          "/resourceGroups/nodecloud/providers/Microsoft.Network/networkInterfaces/nodecloud-interface"
       }
-     },
-    networkProfile: {
-      networkInterfaces: [
-        {
-          id: '/subscriptions/'+ process.env.AZURE_SUBSCRIPTION_ID +'/resourceGroups/nodecloud/providers/Microsoft.Network/networkInterfaces/nodecloud-interface'
-        }
-      ]
-    },
-  };
+    ]
+  }
+};
 
-vm.createOrUpdate(resourceGroupName, vmName, params)
-  .then((res) => {
+vm
+  .createOrUpdate(resourceGroupName, vmName, params)
+  .then(res => {
     return vm.list(resourceGroupName);
   })
-  .then((res) => {
+  .then(res => {
+    return vm.start(resourceGroupName, vmName);
+  })
+  .then(res => {
+    return vm.reboot(resourceGroupName, vmName);
+  })
+  .then(res => {
+    return vm.stop(resourceGroupName, vmName);
+  })
+  .then(res => {
+    return vm.destroy(resourceGroupName, vmName);
+  })
+  .then(res => {
     console.log(res);
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(err);
   });
---
+++ examples/storage/azure_blob_storage.js
@@ -0,0 +1,32 @@
+const nodeCloud = require("../../lib/");
+
+const azure = nodeCloud.getProvider("Azure", null);
+
+const blob = azure.blob();
+
+const containerName = "nodecloud-storage";
+
+const params = {
+  publicAccessLevel: "blob"
+};
+
+blob
+  .create(containerName, params)
+  .then(res => {
+    console.log(res);
+    return blob.list(null, {});
+  })
+  .then(res => {
+    console.log(res);
+    return blob.upload(containerName, "test-blob", "./package.json", {});
+  })
+  .then(res => {
+    console.log(res);
+    return blob.delete(containerName, params);
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.error(err);
+  });
